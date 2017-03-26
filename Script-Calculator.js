05aa51e57ce6b09c8431947cd82babbe72dbb1e5
sunnybpatel <patel.2222@osu.edu>
2017-03-26 17:40:11 -0400

    Added clear function. Fixed numbers for addListeners.

--- Script-Calculator.js
+++ Script-Calculator.js
@@ -15,6 +15,17 @@ var Calculator = function() {
 
     /*
 	Author: Sunny Patel 3/26
+	Clears the display and reset the expressionValue
+	this.display.value = "";
+	this.expressionValue = 0;
+    */
+    this.clear = function() {
+    	this.updateDisplay("");
+    	this.expressionValue = 0;
+    }
+
+    /*
+	Author: Sunny Patel 3/26
 	Updates the expression value based off the input digit
 	this.expressionValue = #this.expresssionValue*10 + digit
     */
@@ -27,27 +38,46 @@ var Calculator = function() {
 };
 
 /*
-Author: Sunny Patel 3/26
+Author: Sunny Patel & Jenn Alarcon3/26
 Adds event listeners to calc's buttons.
 */
 var addListeners = function(calc) {
-	// add event listener for equals button
+	/*
+	add event listener for = button
+	*/
 	var equals = calc.buttons.equal;
 	var equalsFunc = function() {
 		calc.updateDisplay(calc.expressionValue);
 	}
    	equals.addEventListener("click", equalsFunc, false);
-   	// add event listener for each number button
+   	/*add event listener for CLEAR button
+   	*/
+   	var clear = calc.buttons.clear;
+   	var clearFunc = function() {
+   		calc.clear();
+   	}
+   	clear.addEventListener("click", clearFunc, false);
+   	/*
+   	add event listener for each NUMBER button
+   	*/
    	// retrieve all the elements with the name numbers. This is
    	// expected to be all the form elements that are numbers.
-   	for (var i=0; i < calc.buttons.numbers.length; i++) {
-   		var element = calc.buttons.numbers[i];
-		var numberFunc = function() {
-    	    calc.updateExpressionValue(element.value);
-	        calc.updateDisplay(calc.expressionValue);
+   	var funcs = [];
+   	var numberFunc = function(i) {
+   		return function() {
+   			calc.updateExpressionValue(i);
+   			calc.updateDisplay(calc.expressionValue);
    		}
-   		element.addEventListener("click", numberFunc, false);
+   	}
+   	// create 10 functions that print display the values 0-9 to display
+   	for (var i=0; i < calc.buttons.numbers.length; i++) {
+   		funcs[i] = numberFunc(i);
+   	}
+   	// assign those 10 functions to appropriate buttons
+   	for (var j=0; j < calc.buttons.numbers.length; j++) {
+   		var element = calc.buttons.numbers[j];
+   		element.addEventListener("click", funcs[element.value], false);
    	}
 }
 var calc = new Calculator();
-calc.addListeners();
+addListeners(calc);
