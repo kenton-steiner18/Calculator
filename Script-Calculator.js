3e34e42cef04fb290bff7783af0f900394fcec9c
sunnybpatel <patel.2222@osu.edu>
2017-03-26 16:42:30 -0400

    addListeners still not working. Added loop so that once working, we can add listeners for numbers

--- Script-Calculator.js
+++ Script-Calculator.js
@@ -4,7 +4,7 @@ Author: Sunny Patel 3/26
 var Calculator = function() {
 	this.display = document.getElementById("display").display;
 	this.buttons = document.getElementById("mainbuttons")
-	this.expressionValue;
+	this.expressionValue = 0;
 	/*
 	Author: Sunny Patel 3/26
 	Replaces the textfield in the display with input str.
@@ -27,11 +27,28 @@ var Calculator = function() {
 	Author: Sunny Patel 3/26
     */
     this.addListeners = function() {
-    	var e = this.buttons.equal;
-    	e.addEventListener("click", function()
-    	{this.updateDisplay(this.expressionValue)}, false);
-
+    	// add event listener for equals button
+    	var equals = this.buttons.equal;
+		var equalsFunc = function() {
+			this.updateDisplay(this.expressionValue);
+		}
+    	equals.addEventListener("click", equalsFunc, false);
+    	// add event listener for each number button
+    	var numberFunc = function() {
+    		this.updateExpressionValue(element.value);
+    		this.updateDisplay(this.expressionValue);
+    	}
+    	// retrieve all the elements with the name numbers. This is
+    	// expected to be all the form elements that are numbers.
+    	
+    	var numbers = document.getElementsByName("numbers");
+    	for (var i=0; i < this.buttons.numbers.length; i++) {
+      		element = this.buttons.numbers[i];
+    		element.addEventListener("click", numberFunc, false);
+    	}
+    	
     }
 };
 
 var calc = new Calculator();
+calc.addListeners();
