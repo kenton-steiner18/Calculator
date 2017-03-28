e969e69af52d642b40c902231cad3eb71a2d7733
jennalarcon <alarcon.11@osu.edu>
2017-03-28 16:16:23 -0400

    Fixed indenting probz to clean up code.

--- Script-Calculator.js
+++ Script-Calculator.js
@@ -1,7 +1,7 @@
 /*
 Author: Sunny Patel 3/26
 Modified: Jenn Alarcon 3/26
-  Chaged Expression inttial value
+Chaged Expression inttial value
 */
 
 var Calculator = function() {
@@ -12,32 +12,32 @@ var Calculator = function() {
 	Author: Sunny Patel 3/26
 	Replaces the textfield in the display with input str.
 	*/
-    	this.updateDisplay = function(str) {
-    	this.display.value = str;
-    }
+	this.updateDisplay = function(str) {
+		this.display.value = str;
+	}
 
-    	/*
+	/*
 	Author: Sunny Patel 3/26
 	Clears the display and reset the expressionValue
 	this.display.value = "";
 	this.expressionValue = "";
-    	*/
-    	this.clear = function() {
-    	this.updateDisplay("");
-    	this.expressionValue = "";
-    }
+	*/
+	this.clear = function() {
+		this.updateDisplay("");
+		this.expressionValue = "";
+	}
 
-    /*
+	/*
 	Author: Sunny Patel 3/26
 	Updates the expression value based off the input digit
 	this.expressionValue = #this.expresssionValue*10 + digit
 
-  	Modified: Jenn Alarcon 3/26
-  	Wasn't adding digits right when multiplying by ten. -Minor Change, to just add digit to end of string
-    */
+	Modified: Jenn Alarcon 3/26
+	Wasn't adding digits right when multiplying by ten. -Minor Change, to just add digit to end of string
+	*/
 	this.updateExpressionValue = function(digit) {
-	this.expressionValue += digit;
-    }
+		this.expressionValue += digit;
+	}
 
 };
 
@@ -61,50 +61,50 @@ var addListeners = function(calc) {
 
 	dot.addEventListener("click", dotFunc, false);
 
-	
+
 	/*
 	add event listener for = button
 	*/
 	var equals = calc.buttons.equal;
 	var equalsFunc = function() {
-    		calc.expressionValue = eval(calc.expressionValue);
+		calc.expressionValue = eval(calc.expressionValue);
 		calc.updateDisplay(calc.expressionValue);
 	}
-   	equals.addEventListener("click", equalsFunc, false);
+	equals.addEventListener("click", equalsFunc, false);
 
 
-   	/*
-    add event listener for CLEAR button
-   	*/
+	/*
+	add event listener for CLEAR button
+	*/
 	// prevent there are more than one dots for one number
-   	var clear = calc.buttons.clear;
+	var clear = calc.buttons.clear;
 
-   	var clearFunc = function() {
-   		calc.clear();
+	var clearFunc = function() {
+		calc.clear();
 		checkDot = true;
-   	}
-   	clear.addEventListener("click", clearFunc, false);
+	}
+	clear.addEventListener("click", clearFunc, false);
 
 
-   	/*
-   	add event listener for each NUMBER button
-   	*/
-    	var numberFunc = function() {
-         	//window.alert("You pressed: "+this.value);
-    	    	calc.updateExpressionValue(this.value);
-	        calc.updateDisplay(calc.expressionValue);
-    	}
-   	// retrieve all the elements with the name numbers. This is
-   	// expected to be all the form elements that are numbers.
-    	for (var i=0; i < calc.buttons.numbers.length; i++) {
-   		var element = calc.buttons.numbers[i];
-   		element.addEventListener("click", numberFunc, false);
-    	}
+	/*
+	add event listener for each NUMBER button
+	*/
+	var numberFunc = function() {
+		//window.alert("You pressed: "+this.value);
+		calc.updateExpressionValue(this.value);
+		calc.updateDisplay(calc.expressionValue);
+	}
+	// retrieve all the elements with the name numbers. This is
+	// expected to be all the form elements that are numbers.
+	for (var i=0; i < calc.buttons.numbers.length; i++) {
+		var element = calc.buttons.numbers[i];
+		element.addEventListener("click", numberFunc, false);
+	}
 
-    	/*
-    	add event listener for each OPS button
-    	*/
-    	var opsFunc = function(){
+	/*
+	add event listener for each OPS button
+	*/
+	var opsFunc = function(){
 
 		// check whether there is just one OPS between two numbers, overwrite the first OPS
 		var check = true;
@@ -112,7 +112,7 @@ var addListeners = function(calc) {
 		var ops = ["+", "-", "*", "/"];
 		for (var i =0; i < ops.length && check; i++){
 			if(calc.expressionValue.length > 0 && calc.expressionValue.charAt(calc.expressionValue.length - 1) == ops[i]){
-				calc.expressionValue = calc.expressionValue.substring(0, calc.expressionValue.length - 1);	
+				calc.expressionValue = calc.expressionValue.substring(0, calc.expressionValue.length - 1);
 			}
 		}
 
@@ -123,15 +123,15 @@ var addListeners = function(calc) {
 
 		if (check){
 			calc.expressionValue += this.value;
-        		calc.updateDisplay(calc.expressionValue);
+			calc.updateDisplay(calc.expressionValue);
 			checkDot = true;
 		}
 
-    	}
-    	for (var i=0; i < calc.buttons.ops.length; i++) {
-   		var element = calc.buttons.ops[i];
-   		element.addEventListener("click", opsFunc, false);
-    	}
+	}
+	for (var i=0; i < calc.buttons.ops.length; i++) {
+		var element = calc.buttons.ops[i];
+		element.addEventListener("click", opsFunc, false);
+	}
 
 
 }
