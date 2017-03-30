b1dd52223027caad2d6cbfae24fcb294b5d9e8a4
Kenton Steiner <steiner.257@osu.edu>
2017-03-30 00:30:28 -0400

    Edited code to be more terse, changed the checking for the precense of a decimal point to use indexOf

--- Script-Calculator.js
+++ Script-Calculator.js
@@ -9,7 +9,7 @@ var Calculator = function() {
 
 	this.memory = document.getElementById("display").memory;
 
-	this.buttons = document.getElementById("mainbuttons")
+	this.buttons = document.getElementById("mainbuttons");
 
 	this.displayValue = "";
 	this.currentNumber = "";
@@ -25,7 +25,7 @@ var Calculator = function() {
 	Author: Sunny Patel 3/26
 	Replaces the textfield in the display with input str.
 	*/
-	this.updateDisplay = function(str) {
+	this.updateCalcDisplay = function(str) {
 		this.display.value = str;
 	}
 
@@ -48,7 +48,7 @@ var Calculator = function() {
 	*/
 
 	this.clear = function() {
-		this.updateDisplay("");
+		this.updateCalcDisplay("");
 		this.updateMemory("");
 
 		this.displayValue = "";
@@ -88,8 +88,8 @@ var Calculator = function() {
 
 		var op = this.ops.pop();
 
-		var num2 = parseInt(this.numbers.shift());
-		var num1 = parseInt(this.numbers.shift());
+		var num2 = parseFloat(this.numbers.shift());
+		var num1 = parseFloat(this.numbers.shift());
 
 		if(op == "+"){
 			this.evalExpression = num1 + num2;
@@ -110,134 +110,130 @@ var Calculator = function() {
 };
 
 /*
-Author: Sunny Patel & Jenn Alarcon & Raphael Huang 3/26
+Author: Sunny Patel & Jenn Alarcon & Raphael Huang & Kenton Steiner 3/26
 Adds event listeners to calc's buttons.
 */
 var addListeners = function(calc) {
 	/*
-	add event listener for . button
+	add event listener for the decimal point
+	Modified 3/29: Kenton Steiner
+	Removed the checkDot value and used indexOf to determine if the decimal
+	point was already present in the current value.
 	*/
-	var checkDot = true;
 	var dot = calc.buttons.dot;
 	var dotFunc = function() {
-		if (checkDot){
-			calc.displayValue += this.value;
-			calc.updateDisplay(calc.displayValue);
-			checkDot = false;
+		if (calc.displayValue.indexOf('.') == -1) {
+					calc.displayValue += this.value;
+					calc.updateCalcDisplay(calc.displayValue);
 		}
 	}
-
-	dot.addEventListener("click", dotFunc, false);
-
-
-	/*
-	add event listener for = button
-	*/
-	var equals = calc.buttons.equal;
-	var equalsFunc = function() {
-		//add to numbers stack
-		calc.numbers.unshift(calc.currentNumber);
-		calc.displayValue += " ";
-
-		if(calc.numbers.length == 2){
-			//window.alert(calc.ops.length);
-			calc.evaluateExpression();
-			//user need to enter a number
-			calc.displayValue += " ";
-
-		}
-		calc.updateMemory("");
-		calc.updateDisplay(calc.displayValue);
-		calc.lastOpEquals = true;
-
-	}
-	equals.addEventListener("click", equalsFunc, false);
-
-
-	/*
-	add event listener for CLEAR button
-	*/
-	// prevent there are more than one dots for one number
-	var clear = calc.buttons.clear;
-
-	var clearFunc = function() {
-		calc.clear();
-		checkDot = true;
-	}
-	clear.addEventListener("click", clearFunc, false);
-
-
-	/*
-	add event listener for each NUMBER button
-	*/
-	var numberFunc = function() {
-		//window.alert("You pressed: "+this.value);
-		calc.updatedisplayValue(this.value);
-		calc.updateDisplay(calc.displayValue);
-	}
-	// retrieve all the elements with the name numbers. This is
-	// expected to be all the form elements that are numbers.
-	for (var i=0; i < calc.buttons.numbers.length; i++) {
-		var element = calc.buttons.numbers[i];
-		element.addEventListener("click", numberFunc, false);
-	}
-
-	/*
-	add event listener for each OPS button
-	*/
-	var opsFunc = function(){
-
-		// check whether there is just one OPS between two numbers, overwrite the first OPS
-		var check = true;
-
-		var ops = ["+", "-", "*", "/"];
-		for (var i =0; i < ops.length && check; i++){
-			if(calc.displayValue.length > 0 && calc.displayValue.charAt(calc.displayValue.length - 1) == ops[i]){
-				calc.displayValue = calc.displayValue.substring(0, calc.displayValue.length - 1);
-			}
-		}
-
-		// at the beginning, the OPS just can be "-",
-		if(calc.displayValue.length == 0 && this.value != "-"){
-			check = false;
-		}
-
-
-		//***Author: Jenn ****
-		//***UPDATE DISPLAY****/
-		if (check){
-
-
+		
+		dot.addEventListener("click", dotFunc, false);
+	
+	
+		/*
+		add event listener for = button
+		*/
+		var equals = calc.buttons.equal;
+		var equalsFunc = function() {
 			//add to numbers stack
 			calc.numbers.unshift(calc.currentNumber);
-			//clear current number
-			calc.currentNumber ="";
-			//add space after to display space betweeen current and next num
 			calc.displayValue += " ";
-
-			//push op onto the stack
-			calc.ops.unshift(this.value);
-
-			//if there is two values on the stack
+	
 			if(calc.numbers.length == 2){
 				//window.alert(calc.ops.length);
 				calc.evaluateExpression();
 				//user need to enter a number
 				calc.displayValue += " ";
-
+	
 			}
-			//update memory with the op chosen next
-			calc.updateMemory(this.value);
-			calc.updateDisplay(calc.displayValue);
-			checkDot = true;
+			calc.updateMemory(calc.displayValue);
+			calc.updateCalcDisplay("");
+			calc.lastOpEquals = true;
+	
 		}
-
+		equals.addEventListener("click", equalsFunc, false);
+	
+	
+		/*
+		add event listener for CLEAR button
+		*/
+		// prevent there are more than one dots for one number
+		var clear = calc.buttons.clear;
+	
+		var clearFunc = function() {
+			calc.clear();
+		}
+		clear.addEventListener("click", clearFunc, false);
+	
+	
+		/*
+		add event listener for each NUMBER button
+		*/
+		var numberFunc = function() {
+			
+			calc.updatedisplayValue(this.value);
+			calc.updateCalcDisplay(calc.displayValue);
+		}
+		// retrieve all the elements with the name numbers. This is
+		// expected to be all the form elements that are numbers.
+		for (var i=0; i < calc.buttons.numbers.length; i++) {
+			calc.buttons.numbers[i].addEventListener("click", numberFunc, false);
+		}
+	
+		/*
+		add event listener for each OPS button
+		*/
+		var opsFunc = function(){
+	
+			// check whether there is just one OPS between two numbers, overwrite the first OPS
+			var check = true;
+	
+			var ops = ["+", "-", "*", "/"];
+			for (var i =0; i < ops.length && check; i++){
+				if(calc.displayValue.length > 0 && calc.displayValue.charAt(calc.displayValue.length - 1) == ops[i]){
+					calc.displayValue = calc.displayValue.substring(0, calc.displayValue.length - 1);
+				}
+			}
+	
+			// at the beginning, the OPS just can be "-",
+			if(calc.displayValue.length == 0 && this.value != "-"){
+				check = false;
+			}
+	
+	
+			//***Author: Jenn ****
+			//***UPDATE DISPLAY****/
+			if (check){
+	
+	
+				//add to numbers stack
+				calc.numbers.unshift(calc.currentNumber);
+				//clear current number
+				calc.currentNumber ="";
+				//add space after to display space betweeen current and next num
+				calc.displayValue += " ";
+	
+				//push op onto the stack
+				calc.ops.unshift(this.value);
+	
+				//if there is two values on the stack
+				if(calc.numbers.length == 2){
+					//window.alert(calc.ops.length);
+					calc.evaluateExpression();
+					//user need to enter a number
+					calc.displayValue += " ";
+	
+				}
+				//update memory with the op chosen next
+				calc.updateMemory(this.value);
+				calc.updateCalcDisplay(calc.displayValue);
+			}
+	
 	} //END OF OPS FUNCTION
 
-
 	for (var i=0; i < calc.buttons.ops.length; i++) {
-		var element = calc.buttons.ops[i];
-		element.addEventListener("click", opsFunc, false);
+		calc.buttons.ops[i].addEventListener("click", opsFunc, false);
 	}
 	//***Author: Jenn ****
 
